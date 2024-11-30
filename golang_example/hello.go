5ed3e11865cb53bcfb38d7c568602779e1048324
RyanWalker-FS <ryanwalker490@gmail.com>
2024-11-30 12:29:02 -0500

    Go upload

--- golang_example/hello.go
+++ golang_example/hello.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-  "fmt"
-  "time"
+    "fmt"
+    "time"
 )
 
 func main() {
-  fmt.Println("Hello ASL!")
-  fmt.Printf("Current Date: %s\n", time.Now().Format("2006-01-02 15:04:05"))}
+    fmt.Println("Hello ASL!")
+    fmt.Printf("Current Date: %s\n", time.Now().Format("2006-01-02"))
+}
+
--- ruby_example/hello.rb
+++ ruby_example/hello.rb
@@ -1,2 +1,3 @@
+
 puts "Hello ASL!"
 puts "Current Date: #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}"
