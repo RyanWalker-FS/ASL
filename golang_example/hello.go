3f81a88c62eb07b6b3045e06d0803b30ae899065
RyanWalker-FS <ryanwalker490@gmail.com>
2024-11-30 10:30:01 -0500

    ruby/golang push

---
+++ golang_example/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  fmt.Println("Hello ASL!")
+  fmt.Printf("Current Date: %s\n", time.Now().Format("2006-01-02 15:04:05"))}
---
+++ ruby_example/hello.rb
@@ -0,0 +1,2 @@
+puts "Hello ASL!"
+puts "Current Date: #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}"
